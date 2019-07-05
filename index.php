a322a1eb49e5400e7929da28c29dae9a11936a72
d4admin_yuls <d4a@d4a.com>
2019-07-05 15:06:37 -0500

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$enlace =  mysql_connect('mysql', 'root', 'TM1q2w3e.');
+if (!$enlace) {
+    die('No pudo conectarse: ' . mysql_error());
+}
+echo 'Conectado satisfactoriamente';
+mysql_close($enlace);
+?>
+
+
+Hola mundo en docker-compose
